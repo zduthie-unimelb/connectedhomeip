e063adc1aa47f26865476dd5c3590b8ad3380dbc
srkelly-unimelb <srkelly@student.unimelb.edu.au>
2023-05-20 14:21:10 +0000

    Bonus all-clusters fuzz driver (makes for quicker local debugging)

--- examples/all-clusters-app/linux/fuzzing-main-1.cpp
+++ examples/all-clusters-app/linux/fuzzing-main-1.cpp
@@ -51,7 +51,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * aData, size_t aSize)
 {
 
 #if CHIP_CONFIG_SECURITY_FUZZ_LOGGING
-    ChipLogProgress(Test, "<SIKE-0>fuzzing-main");
+    ChipLogProgress(Test, "<SIKE-1>fuzzing-main");
     chip::Encoding::LogBufferAsHex("LLVMFuzzerTestOneInput", chip::ByteSpan(aData, aSize));
 #endif
 
@@ -81,6 +81,36 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * aData, size_t aSize)
     static bool matterStackInitialized = false;
     if (!matterStackInitialized)
     {
+
+        // Debug #defines
+        std::cerr << "   *** Initialising:";
+        
+        #ifdef CHIP_CONFIG_SECURITY_TEST_MODE
+        std::cerr << " CHIP_CONFIG_SECURITY_TEST_MODE=" << CHIP_CONFIG_SECURITY_TEST_MODE;
+        #else
+        std::cerr << " CHIP_CONFIG_SECURITY_TEST_MODE=Undefined"
+        #endif
+
+        #ifdef CHIP_CONFIG_SECURITY_FUZZ_MODE
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_MODE=" << CHIP_CONFIG_SECURITY_FUZZ_MODE;
+        #else
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_MODE=Undefined"
+        #endif
+
+        #ifdef CHIP_CONFIG_SECURITY_FUZZ_LOGGING
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_LOGGING=" << CHIP_CONFIG_SECURITY_FUZZ_LOGGING;
+        #else
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_LOGGING=Undefined"
+        #endif
+
+        #ifdef CHIP_CONFIG_SECURITY_FUZZ_SEED_BUG_1
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_SEED_BUG_1=" << CHIP_CONFIG_SECURITY_FUZZ_SEED_BUG_1;
+        #else
+        std::cerr << " CHIP_CONFIG_SECURITY_FUZZ_SEED_BUG_1=Undefined"
+        #endif
+
+        std::cerr << " *** " << std::endl; 
+
         // Might be simpler to do ChipLinuxAppInit() with argc == 0, argv set to
         // just a fake executable name?
         VerifyOrDie(Platform::MemoryInit() == CHIP_NO_ERROR);
@@ -119,6 +149,50 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * aData, size_t aSize)
         // Too big; we couldn't represent this as a packetbuffer to start with.
         return 0;
     }
+
+    // // Wireshark: Copy as escaped string
+// Step1
+const char *packet1 = "\x04\x00\x00\x00\xf8\x8a\x34\x05\x13\xdb\x8a\xfa\x59\x1c\xfe\xca" \
+    "\x05\x20\x1f\x7d\x00\x00\x15\x30\x01\x20\x63\xfb\x3b\xee\x65\xbf" \
+    "\xfb\x11\xc0\xd4\xde\x2c\x18\xdd\x45\x0c\x6d\x9c\xee\x99\xb4\xd0" \
+    "\x9c\x01\x16\xda\xba\xaf\xe4\xeb\x1f\xee\x25\x02\x4e\x17\x24\x03" \
+    "\x00\x28\x04\x18";
+size_t packet1_size = 68;
+
+// // Step2
+// const char *packet2 = "\x04\x00\x00\x00\xf9\x8a\x34\x05\x13\xdb\x8a\xfa\x59\x1c\xfe\xca" \
+//     "\x05\x22\x1f\x7d\x00\x00\x15\x30\x01\x41\x04\x18" \
+//     "\x80\xbf\xd6\xf6\x1c\xd7\x64\xb1\x5d\x78\xa9\x32\x33\xd3\x48\x85" \
+//     "\x6a\xb1\x01\xfe\x3c\x46\x51\x7d\xbe\xf1\x88\x63\x77\xb6\x25\x9c" \
+//     "\x16\x54\x69\xbd\x57\x98\xb2\x1f\x37\xac\xe7\x17\x74\xda\x81\x06" \
+//     "\x14\x88\xae\xbb\x63\xff\x53\x3c\xd9\x7d\x86\x4e\x95\x9b\xc3\x18";
+// size_t packet2_size = 92;
+
+// Step2
+const char *packet2 = "\x04\x00\x00\x00\xf9\x8a\x34\x05\x13\xdb\x8a\xfa\x59\x1c\xfe\xca" \
+    "\x05\x22\x1f\x7d\x00\x00\x5c\x1b\x42\x05\x15\x30\x01\x41\x04\x18" \
+    "\x80\xbf\xd6\xf6\x1c\xd7\x64\xb1\x5d\x78\xa9\x32\x33\xd3\x48\x85" \
+    "\x6a\xb1\x01\xfe\x3c\x46\x51\x7d\xbe\xf1\x88\x63\x77\xb6\x25\x9c" \
+    "\x16\x54\x69\xbd\x57\x98\xb2\x1f\x37\xac\xe7\x17\x74\xda\x81\x06" \
+    "\x14\x88\xae\xbb\x63\xff\x53\x3c\xd9\x7d\x86\x4e\x95\x9b\xc3\x18";
+size_t packet2_size = 96;
+
+// const char *packet2 = "\x04\x00\x00\x00\xf9\x8a\x34\x05\x13\xdb\x8a\xfa\x59\x1c\xfe\xca" \
+// "\x07\x22\x1f\x7d\x00\x00\x5c\x1b\x42\x05\x15\x30\x01\x41\x04\x18" \
+// "\x80\xbf\xd6\xf6\x1c\xd7\x64\xb1\x5d\x78\xa9\x32\x33\xd3\x48\x85" \
+// "\x6a\xb1\x01\xfe\x3c\x46\x51\x7d\xbe\xf1\x88\x63\x77\xb6\x25\x9c" \
+// "\x16\x54\x69\xbd\x57\x98\xb2\x1f\x37\xac\xe7\x17\x74\xda\x81\x06" \
+// "\x14\x88\xae\xbb\x63\xff\x53\x3c\xd9\x7d\x86\x4e\x95\x9b\xc3\x18";
+// size_t packet2_size = 96;
+
+// // VerifyOrDie(false);
+System::PacketBufferHandle buf_1 =
+    System::PacketBufferHandle::NewWithData(packet1, packet1_size, 0, 0);
+// Server::GetInstance().GetSecureSessionManager().OnMessageReceived(peerAddr, std::move(buf_1));
+
+System::PacketBufferHandle buf_2 =
+    System::PacketBufferHandle::NewWithData(packet2, packet2_size, 0, 0);
+// Server::GetInstance().GetSecureSessionManager().OnMessageReceived(peerAddr, std::move(buf_2));
 
     // Ignoring the return value from OnMessageReceived, because we might be
     // passing it all sorts of garbage that will cause it to fail.
